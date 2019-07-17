d8da4b35f9e9a9ae9af1605402d5428dbcfdaf95
Maxim <maxim@trackpro.lv>
2019-07-17 11:29:34 +0300

    version 1.0.6 - outdated chrome error fix - electron update

--- main.js
+++ main.js
@@ -1,66 +1,90 @@
-const { app, BrowserWindow, Tray, Menu } = require('electron')
-const path = require('path')
-const url = require('url')
+const {app, BrowserWindow, Tray, Menu} = require('electron');
+const path = require('path');
 
-const iconGreen = path.join(__dirname, 'icon.ico')
-const iconBlack = path.join(__dirname, 'black.ico')
+const iconGreen = path.join(__dirname, 'icon.ico');
+const iconBlack = path.join(__dirname, 'black.ico');
 
-let window
-let notifyTimer
-let notifyIconIsBlack = false
+let win;
+let notifyTimer;
+let notifyIconIsBlack = false;
 
 function start() {
-    window = new BrowserWindow({ 
-        width: 1200, 
-        height: 750, 
-        icon: iconGreen, 
-        title: 'WhatsApp' 
-    })
+    win = new BrowserWindow({
+        width: 950,
+        height: 600,
+        icon: iconGreen,
+        title: 'WhatsApp',
+        visible: true,
+        webPreferences: {
+            nodeIntegration: true,
+            partition: "persist:main",
+            webSecurity: false
+        }
+    });
 
-    window.loadURL('https://web.whatsapp.com/')
+    win.webContents.setUserAgent(win.webContents.getUserAgent().replace(/(Electron|Chrome)\/([0-9\.]+)\ /g, ""));
+    win.loadURL('https://web.whatsapp.com/', {userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'});
 
-    window.on('page-title-updated', function (e, title) {
+    win.webContents.on('did-finish-load', function () {
+        win.webContents.executeJavaScript(`
+            window.navigator.serviceWorker.getRegistrations().then(registrations => {
+                for (let registration of registrations) {
+                    registration.unregister();
+                }
+            });
+            const titleEl = document.querySelector('.window-title');
+            if (titleEl && titleEl.innerHTML.includes('Google Chrome 36+')) {
+                window.location.reload();
+            }
+        `);
+    });
+
+    win.on('closed', () => {
+        win = null;
+    });
+
+    win.on('page-title-updated', function (e, title) {
         if (title !== 'WhatsApp') {
-            icon.setImage(iconBlack)
+            icon.setImage(iconBlack);
             if (notifyTimer) {
-                clearInterval(notifyTimer)
+                clearInterval(notifyTimer);
             }
             notifyTimer = setInterval(function () {
-                icon.setImage(notifyIconIsBlack ? iconBlack : iconGreen)
-                notifyIconIsBlack = !notifyIconIsBlack
+                icon.setImage(notifyIconIsBlack ? iconBlack : iconGreen);
+                notifyIconIsBlack = !notifyIconIsBlack;
             }, 1000)
         } else {
-            icon.setImage(iconGreen)
+            icon.setImage(iconGreen);
             if (notifyTimer) {
-                clearInterval(notifyTimer)
+                clearInterval(notifyTimer);
             }
         }
-    })
+    });
 
-    window.on('show', function () {
-        icon.setHighlightMode('always')
-    })
+    win.on('show', function () {
+        icon.setHighlightMode('always');
+    });
 
-    window.on('minimize', function (event) {
-        event.preventDefault()
-        window.minimize()
-    })
+    win.on('minimize', function (event) {
+        event.preventDefault();
+        win.minimize();
+    });
 
-    window.on('close', function (event) {
-        event.preventDefault()
-        window.hide()
+    win.on('close', function (event) {
+        event.preventDefault();
+        win.hide();
         return false
-    })
+    });
 
-    window.on('closed', function () {
+    win.on('closed', function () {
         app.quit()
-    })
+    });
 
-    var menu = Menu.buildFromTemplate([
+    const menu = Menu.buildFromTemplate([
         {
             label: 'WhatsApp',
             click: function () {
-                window.show()
+                win.show()
             }
         },
         {
@@ -69,14 +93,14 @@ function start() {
                 app.exit()
             }
         }
-    ])
+    ]);
 
-    var icon = new Tray(iconGreen)
+    const icon = new Tray(iconGreen);
     icon.setToolTip('WhatsApp');
-    icon.setContextMenu(menu)
+    icon.setContextMenu(menu);
     icon.on('double-click', function () {
-        window.show()
-    })
+        win.show();
+    });
 }
 
-app.on('ready', start)
+app.on('ready', start);
